fd215ae8e7a7ccce6103396f043718f08c0cd74f
JayathriRanasinghe <e18283@eng.pdn.ac.lk>
2024-03-27 21:46:20 +0530

    initial backend application setup

---
+++ Backend-Social-Service/Backend-Social-Service/src/main/java/com/jaya3/BackendSocialService/BackendSocialServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jaya3.BackendSocialService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSocialServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSocialServiceApplication.class, args);
+	}
+
+}
---
+++ Backend-Social-Service/Backend-Social-Service/src/test/java/com/jaya3/BackendSocialService/BackendSocialServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaya3.BackendSocialService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSocialServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
