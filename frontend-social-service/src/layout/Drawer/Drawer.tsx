4b4d17035bbf95c1cb82bebf906e3008af379e86
JayathriRanasinghe <e18283@eng.pdn.ac.lk>
2024-04-02 23:55:35 +0530

    updated the drawer

--- frontend-social-service/src/layout/Drawer/Drawer.tsx
+++ frontend-social-service/src/layout/Drawer/Drawer.tsx
@@ -8,8 +8,13 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import GroupIcon from '@mui/icons-material/Group';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import FuneralIcon from '@mui/icons-material/EmojiPeople';
+import BillIcon from '@mui/icons-material/Receipt';
+import OtherExpensesIcon from '@mui/icons-material/ReceiptLong';
 
 export default function TemporaryDrawer() {
   const [open, setOpen] = React.useState(false);
@@ -18,14 +23,28 @@ export default function TemporaryDrawer() {
     setOpen(newOpen);
   };
 
+  const icons1 = [
+    <PersonAddIcon />,
+    <GroupIcon />
+  ];
+
+  const icons2 = [
+    <AttachMoneyIcon />,
+    <InventoryIcon />,
+    <FuneralIcon />,
+    <BillIcon />,
+    <OtherExpensesIcon />
+  ];
+
+
   const DrawerList = (
     <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+        {['Member Registration', 'Members'].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              {icons1[index]}
               </ListItemIcon>
               <ListItemText primary={text} />
             </ListItemButton>
@@ -34,11 +53,11 @@ export default function TemporaryDrawer() {
       </List>
       <Divider />
       <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+        {['Member Fees', 'Inventory Fees', 'Funeral Assistance' ,'Bill Logging', 'Other Expenses'].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              {icons2[index]}
               </ListItemIcon>
               <ListItemText primary={text} />
             </ListItemButton>
